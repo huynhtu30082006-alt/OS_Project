fc945a56666c55cd9ad7f5883d847540a2e95f5b
rin-ikiri <vanlinhtran50@gmail.com>
2026-01-04 16:54:17 +0700

    update

--- scheduling_algorithms.h
+++ scheduling_algorithms.h
@@ -1,29 +1,5 @@
 #ifndef SCHEDULING_ALGORITHMS_H
 #define SCHEDULING_ALGORITHMS_H
 
-#include <vector>
-#include <string>
-#include <algorithm>
-
-using namespace std;
-
-struct Process {
-    string id;
-    int arrivalTime;
-    int burstTime;
-    int startTime;
-    int completionTime;
-    int waitingTime;
-    int turnaroundTime;
-    bool isCompleted = false;
-};
-
-struct SchedulingResult {
-    vector<Process> processes;
-    double avgWaitingTime;
-    double avgTurnaroundTime;
-};
-
-SchedulingResult runSJF(vector<Process> processes);
 
 #endif
